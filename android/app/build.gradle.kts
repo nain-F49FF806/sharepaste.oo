d271b43d3865567b0ee870071e05daf731cf0b26
nain <126972030+nain-F49FF806@users.noreply.github.com>
2024-04-10 12:41:59 +0200

    Shrink output apk size.
    This does reduce it by about 30% . (27M -> 17M)
    
    It could be more effective if there was a way to enable stronger zip compression.
    But I haven't been able to find a way to do so yet.
    
    For now maybe distribute the apk gzipped (apk.gz) to improve compression.
    This reduces size by about another 50 % (output ~ 8.7M)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -47,7 +47,8 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
