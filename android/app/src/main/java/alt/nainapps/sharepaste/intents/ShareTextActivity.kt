1830278f96e2c414f7b7f2355b6ef0442f909950
nain <126972030+nain-F49FF806@users.noreply.github.com>
2024-04-16 08:38:29 +0200

    Implement share from other apps (recieve send text intent)

---
+++ android/app/src/main/java/alt/nainapps/sharepaste/intents/ShareTextActivity.kt
@@ -0,0 +1,48 @@
+package alt.nainapps.sharepaste.intents
+
+import alt.nainapps.sharepaste.common.EncryptAndShareUI
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import alt.nainapps.sharepaste.intents.ui.theme.SharePasteO2Theme
+import android.content.Intent
+
+class ShareTextActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SharePasteO2Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    EncryptAndShareUI(intent?.getStringExtra(Intent.EXTRA_TEXT).orEmpty())
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    SharePasteO2Theme {
+        Greeting("Android")
+    }
+}
