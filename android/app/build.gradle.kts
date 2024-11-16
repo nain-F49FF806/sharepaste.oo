b370090d7c4ec948fd956ee7005a120e4b153537
nain <126972030+nain-F49FF806@users.noreply.github.com>
2024-11-16 13:07:27 +0000

    Upgrade compile sdk

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,13 +7,13 @@ plugins {
 
 android {
     namespace = "alt.nainapps.sharepaste"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "alt.nainapps.sharepaste"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1731761000
+        versionCode = 1731762000
         versionName = "2024.11.16"
         setProperty("archivesBaseName", "sharepaste.oo")
 
